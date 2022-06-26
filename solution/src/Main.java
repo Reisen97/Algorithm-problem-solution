c6576d55ef29746f42062691f9ac79e09983fb37
songha <thdgk333@gmail.com>
2022-06-26 15:42:52 +0900

    test

--- solution/src/Main.java
+++ solution/src/Main.java
@@ -6,25 +6,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(br.readLine());
-        int isCount = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        while (st.hasMoreTokens()) {
-            int temp = Integer.parseInt(st.nextToken());
-            if (temp == 2) {
-                ++isCount;
-            }
-            else if (temp % 2 != 0) {
-                for (int i = temp - 1; i > 1; --i) {
-                    if (temp % i == 0) {
-                        break;
-                    }
-                    if (i == 2) {
-                        ++isCount;
-                    }
-                }
-            }
-        }
-        System.out.print(isCount);
+        System.out.print("change text");
     }
 }
